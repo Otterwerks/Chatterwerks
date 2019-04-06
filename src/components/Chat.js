47022f4e09ef01f891cefdce637f89f0b0988188
Sam Weber <sweber102@gmail.com>
2019-04-05 22:04:21 -0700

    Connecting Redux

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -6,35 +6,39 @@ import Axios from 'axios';
 
 
 const Chat = ({ user, updateMessages }) => (
-    <div className="row">
-        {setInterval(function () {
-            Axios.post('api/v1/messages/query', {
-                user_name: user.userName,
-                user_password: user.userPassword,
-                thread_id: user.currentThread
-            }).then((res) => {
-                if (res.data.response == 'success') {
-                updateMessages(res.data.messages)
-                }
-                console.log(res);
-            })
-            .catch(function (err) {
-                console.log(err);
-            })
-            }, 5000)
-        }
-        <div className="col-3">
-            <UserList />
+    <div>
+        <div className="hidden">
+            {setInterval(function () {
+                Axios.post('api/v1/messages/query', {
+                    user_name: user.userName,
+                    user_password: user.userPassword,
+                    thread_id: user.currentThread
+                }).then((res) => {
+                    if (res.data.response == 'success') {
+                    updateMessages(res.data.messages)
+                    }
+                    console.log(res);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                })
+                }, 5000)
+            }
         </div>
-        <div className="col-9">
-            <div className="row">
-                <div id="chatField" className="col jumbotron chat-window">
-                    <MessageList />
-                </div>
+        <div className="row">
+            <div className="col-3">
+                <UserList />
             </div>
-            <div className="row">
-                <div className="col">
-                    <SubmitMessage />
+            <div className="col-9">
+                <div className="row">
+                    <div id="chatField" className="col jumbotron chat-window">
+                        <MessageList />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <SubmitMessage />
+                    </div>
                 </div>
             </div>
         </div>
