3bf33e1d0e3d3876b41405f9cd5f4891613af93f
Otterwerks <44848311+Otterwerks@users.noreply.github.com>
2019-04-10 17:47:55 -0700

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,5 +1,5 @@
 import main
-import pytes
+import pytest
 import time
 from main import db
 
@@ -9,6 +9,8 @@ test_user_password = "" #testpassword
 
 test_thread_id = -10
 test_thread_name = "" # time.time() / 10
+
+test_subscription_id = -10
 
 
 @pytest.fixture
@@ -34,5 +36,29 @@ def test_resolve_subscription(client):
     assert Get_Subscription_ID(test_user_id + "x", test_thread_id) == "id_not_found"
     assert Get_Subscription_ID() == "id_not_found"
     
+def test_create_subscription(client):
+    assert Subscription.query.filter_by(user_id=-11, thread_id=-11).first() == None
+    Create_Subscription(-11, -11)
+    assert Subscription.query.filter_by(user_id=-11, thread_id=-11).first() not None
+    
+with app.test_client() as c:
+    rv = c.post('/api/v1/users/login', json={
+        'user_name': test_user_name, 'user_password': test_user_password
+    })
+    json_data = rv.get_json()
+    assert rv.get_json()['response'] == 'success'
+    
+with app.test_client() as c:
+    rv = c.post('/api/v1/users/login', json={
+        'user_name': test_user_name, 'user_password': (test_user_password + "x")
+    })
+    json_data = rv.get_json()
+    assert rv.get_json()['response'] == 'incorrect'
 
+with app.test_client() as c:
+    rv = c.post('/api/v1/users/login', json={
+        'user_name': (test_user_name + "x"), 'user_password': (test_user_password + "x")
+    })
+    json_data = rv.get_json()
+    assert rv.get_json()['response'] == 'failed'
     
